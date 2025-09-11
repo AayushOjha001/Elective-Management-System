0badf7f49b3eb296a39a1d475452f8de381cdb41
aaayushpandey <aayushpandey706@gmail.com>
2025-09-11 22:21:33 +0545

    Fix NOT NULL constraint error on ElectiveSubject model
    - Added max_students field to ElectiveSubject model with default=24
    - Added corresponding migration files
    - Updated database backup for distribution
    - Added test Excel file for single subject selection scenario

--- PMS/apps/course/models.py
+++ PMS/apps/course/models.py
@@ -53,6 +53,10 @@ class ElectiveSubject(models.Model):
         default=10,
         validators=[MinValueValidator(5)]
     )
+    max_students = models.PositiveIntegerField(
+        verbose_name='Maximum number of students allowed',
+        default=24
+    )
 
     def __str__(self):
         return self.subject_name
